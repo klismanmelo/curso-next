b80b0e0bf0784b18487a1572e52176c2ae527daa
klismanmelo <61260271+klismanmelo@users.noreply.github.com>
2020-10-04 14:04:56 -0300

    Static Side Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,45 @@
+import { GetServerSideProps } from 'next';
+import { useState, useEffect } from 'react';
+
 import { Title } from '../styles/pages/Home';
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recomendedProducts: IProduct[];
+}
+
+export default function Home({ recomendedProducts }: HomeProps) {
   return (
     <div>
-      <Title>Hello Word</Title>
+
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recomendedProducts.map(recomendedProducts => {
+            return (
+              <li key={recomendedProducts.id}>
+                {recomendedProducts.title}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recomendedProducts = await response.json();
+
+  return {
+    props: {
+      recomendedProducts
+    }
+  }
 }
